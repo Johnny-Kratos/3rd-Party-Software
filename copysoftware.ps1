be62a71457f70a3b74d58406f15ad903d6e1490c
Johnny-Kratos <121155657+Johnny-Kratos@users.noreply.github.com>
2023-12-13 15:00:45 +0800

    Update copysoftware.ps1

--- copysoftware.ps1
+++ copysoftware.ps1
@@ -1,1 +1,2 @@
-﻿robocopy C:\KRATOSFOLDER\software\ "\\172.38.0.2\software" /MIR
+net user \\172.16.10.129\software\ /user:Admin {{ secrets.WINDOWSPASSWORD }}
+robocopy "\\172.16.10.129\software\" "\\172.38.0.2\software" /MIR
